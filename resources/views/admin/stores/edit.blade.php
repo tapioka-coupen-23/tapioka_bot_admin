3955d06ab95d2f3001c66fc537ba4e4fd50265af
Ryosei Takahashi <zexel8716@gmail.com>
2019-09-07 17:01:39 +0900

    storeのview追加

--- resources/views/admin/stores/edit.blade.php
+++ resources/views/admin/stores/edit.blade.php
@@ -1,5 +1,25 @@
-@if(!empty($store))
-    <form action="">
 
-    </form>
-@endif
+
+@extends('dashboard-layout')
+
+@section('content')
+    @if(!empty($store))
+        <form action="{{ route('admin.stores.update', [$store->id]) }}" method="patch">
+    @else
+        <form action="{{ route('admin.stores.create.exec') }}" method="put">
+    @endif
+            <div class="form-group">
+                <label class="col-3 label label-primary">店名</label>
+                <input type="text" class="form-control col-8" name="name" value="{{ !empty($store) ? old('name', $store->name) : old('name') }}">
+            </div>
+            <div class="form-group">
+                <label class="col-3">店舗URL</label>
+                <input type="text" class="form-control col-8" name="url" value="{{ !empty($store) ? old('url', $store->url) : old('url') }}">
+            </div>
+            <div class="form-group">
+                <label class="col-3">説明</label>
+                <textarea class="form-control">{{ !empty($store) ? old('description', $store->description) : old('description') }}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">保存</button>
+        </form>
+@endsection
--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,1 +1,40 @@
-<a href="{{ route('admin.stores.create') }}">作成</a>
+@extends('dashboard-layout')
+
+@section('content')
+    <a href="{{ route('admin.stores.create') }}" class="btn btn-success">作成</a>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th class="text-center">#</th>
+            <th>ユーザー名</th>
+            <th>クーポン</th>
+            <th>都道府県</th>
+            <th class="text-right">年齢</th>
+            <th class="text-right">性別</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($stores as $store)
+                <tr>
+                    <td class="text-center">1</td>
+                    <td>Andrew Mike</td>
+                    <td>Develop</td>
+                    <td>2013</td>
+                    <td class="text-right">&euro; 99,225</td>
+                    <td class="td-actions text-right">
+                        <button type="button" rel="tooltip" class="btn btn-info">
+                            <i class="material-icons">person</i>
+                        </button>
+                        <button type="button" rel="tooltip" class="btn btn-success">
+                            <i class="material-icons">edit</i>
+                        </button>
+                        <button type="button" rel="tooltip" class="btn btn-danger">
+                            <i class="material-icons">close</i>
+                        </button>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
