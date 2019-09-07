aa36bf685f4f555bc52170c15c662f12acfa7d52
秀和 <hidekazu5200@gmail.com>
2019-09-07 16:06:46 +0900

    ダッシュボードの作成

---
+++ resources/views/dashboard-home.blade.php
@@ -0,0 +1,6 @@
+@extends('dashboard-layout')
+
+@section('content')
+<button type="button" class="btn btn-warning">新規クーポン登録</button>
+<button type="button" class="btn btn-warning">店舗登録</button>
+@endsection
